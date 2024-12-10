29485049dc0bb5b5df8acb8ba8004b6bc27464b8
Michael <65353281@qq.com>
2024-12-10 16:32:32 +0800

    红色加力拉取城市代码

--- src/main/java/com/example/basic/service/HsjlCityService.java
+++ src/main/java/com/example/basic/service/HsjlCityService.java
@@ -5,14 +5,17 @@ import com.example.basic.config.HsjlApiConfig;
 import com.example.basic.domain.hsjl.req.HsjlCityReq;
 import com.example.basic.domain.hsjl.resp.HsjlCityResp;
 import com.example.basic.utils.HsjlHttp;
+import com.google.common.collect.Maps;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class HsjlCityService {
 
   public HsjlCityResp getCityList(HsjlCityReq request) {
-    String businessRequest = buildBusinessRequest(request);
+    Map<String, Object> businessRequest = this.buildBusinessRequest(request);
     String responseBody =
         HsjlHttp.httpRequest(HsjlApiConfig.REQUEST_TYPE_HOTEL_CITY, businessRequest);
     if (StringUtils.isEmpty(responseBody)) {
@@ -21,13 +24,10 @@ public class HsjlCityService {
     return JSON.parseObject(responseBody, HsjlCityResp.class);
   }
 
-  private String buildBusinessRequest(HsjlCityReq request) {
-    return "{\"countryCode\":\""
-        + request.getCountryCode()
-        + "\","
-        + "\"haveLowestPrice\":\""
-        + request.isHaveLowestPrice()
-        + "\""
-        + "}}";
+  private Map<String, Object> buildBusinessRequest(HsjlCityReq request) {
+    Map<String, Object> businessRequest = Maps.newHashMap();
+    businessRequest.put("countryCode", request.getCountryCode());
+    businessRequest.put("haveLowestPrice", request.isHaveLowestPrice());
+    return businessRequest;
   }
 }
--- src/main/java/com/example/basic/utils/HsjlHttp.java
+++ src/main/java/com/example/basic/utils/HsjlHttp.java
@@ -1,69 +1,44 @@
 package com.example.basic.utils;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.zip.GZIPInputStream;
-
+import java.util.Map;
 import com.example.basic.config.HsjlApiConfig;
+import com.google.common.collect.Maps;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 public class HsjlHttp {
 
-  public static String httpRequest(String requestType, String businessRequest) {
-    RestTemplate restTemplate = new RestTemplate();
+  public static String httpRequest(String requestType, Map<String, Object> businessRequest) {
 
     // 设置请求头
+    String timestamp = String.valueOf(System.currentTimeMillis());
     HttpHeaders headers = new HttpHeaders();
-    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-    headers.add("Accept-Encoding", "gzip");
+    headers.setContentType(MediaType.APPLICATION_JSON);
     headers.add("Connection", "keep-alive");
-    String timestamp = String.valueOf(System.currentTimeMillis() / 1000L);
-    headers.add("timestamp", timestamp);
-    headers.add("partnerCode", HsjlApiConfig.partnerCode);
-    headers.add("requestType", requestType);
-    headers.add("version", HsjlApiConfig.VERSION);
-    headers.add("gzip", "1");
-    headers.add(
+    headers.set("partnerCode", HsjlApiConfig.partnerCode);
+    headers.set("requestType", requestType);
+    headers.set(
         "signature",
         SignatureUtils.hsjlSignature(
             timestamp, HsjlApiConfig.secureKey, HsjlApiConfig.partnerCode, requestType));
+    headers.set("timestamp", timestamp);
+    headers.set("version", HsjlApiConfig.VERSION);
+    headers.set("gzip", "0");
 
     // 构建请求参数
-    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-    params.add("businessRequest", businessRequest);
+    Map<String, Object> requestBody = Maps.newHashMap();
+    requestBody.put("businessRequest", businessRequest);
+    requestBody.put("header", headers.toSingleValueMap());
 
-    // 构建请求URL
-    URI uri =
-        UriComponentsBuilder.fromUriString(HsjlApiConfig.apiUrl)
-            .queryParams(params)
-            .build()
-            .encode(StandardCharsets.UTF_8)
-            .toUri();
+    // 创建RestTemplate实例
+    RestTemplate restTemplate = new RestTemplate();
 
-    // 发送请求并获取响应
-    ResponseEntity<byte[]> response =
-        restTemplate.exchange(uri, HttpMethod.POST, new HttpEntity<>(headers), byte[].class);
+    // 发送POST请求
+    HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+    ResponseEntity<String> responseEntity =
+        restTemplate.exchange(
+            HsjlApiConfig.apiUrl + requestType, HttpMethod.POST, requestEntity, String.class);
 
-    // 检查响应内容类型
-    if (response.getStatusCode().is2xxSuccessful()) {
-      byte[] responseBodyBytes = response.getBody();
-      if (responseBodyBytes != null) {
-        try (InputStream is = new GZIPInputStream(new ByteArrayInputStream(responseBodyBytes))) {
-          return new String(is.readAllBytes(), StandardCharsets.UTF_8);
-        } catch (IOException e) {
-          throw new RuntimeException("Failed to decompress response: " + e.getMessage(), e);
-        }
-      }
-    } else {
-      throw new RuntimeException("Failed to get response: " + response.getStatusCode());
-    }
-    return null;
+    return responseEntity.getBody();
   }
 }
